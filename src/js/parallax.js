d111a20e91015a989fbf694b1fc81deba3ab0572
Nicholas Ragland <nragland37@gmail.com>
2024-10-08 20:11:09 -0500

    add parallax effect for mobile to mimic desktop

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,13 +1,18 @@
 function applyParallaxEffect() {
-    document.addEventListener('DOMContentLoaded', function () {
-        window.addEventListener('scroll', function () {
-            var scrollPosition = window.pageYOffset;
-            var bgElement = document.querySelector('.header-img');
-            if (bgElement) {
-                bgElement.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
-            }
+    // Check if the screen width is less than or equal to 768 pixels
+    // ONLY ativate for mobile devices
+    // It's a cheeky workaround to get the parallax effect to work on mobile devices
+    if (window.innerWidth <= 768) {
+        document.addEventListener('DOMContentLoaded', function () {
+            window.addEventListener('scroll', function () {
+                var scrollPosition = window.pageYOffset;
+                var bgElement = document.querySelector('.header-img');
+                if (bgElement) {
+                    bgElement.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+                }
+            });
         });
-    });
+    }
 }
 
-applyParallaxEffect(); // Call the function to activate the effect
+applyParallaxEffect(); 
