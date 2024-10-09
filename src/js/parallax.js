700a81d5ce238528404d9e428299dcd4a145e19d
Nicholas Ragland <nragland37@gmail.com>
2024-10-09 14:30:02 -0500

    wee gooo brrrr

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -2,21 +2,21 @@ function applyParallaxEffect() {
     const bgElement = document.querySelector('.header-img');
     
     if (!bgElement) return;
-
+  
     const isMobile = window.innerWidth <= 768;
-
+  
     function updateBackgroundPosition(scrollPosition) {
-        if (isMobile) {
-            bgElement.style.backgroundPositionY = (scrollPosition * 0.3) + 'px';  // Adjust this ratio for a better effect
-        } else {
-            bgElement.style.backgroundAttachment = 'fixed';
-        }
+      if (isMobile) {
+        // Simulate parallax effect by adjusting background position on mobile
+        bgElement.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+      }
     }
-
+  
     window.addEventListener('scroll', function () {
-        const scrollPosition = window.scrollY;
-        updateBackgroundPosition(scrollPosition);
+      const scrollPosition = window.scrollY;
+      updateBackgroundPosition(scrollPosition);
     });
-}
-
-applyParallaxEffect();
+  }
+  
+  applyParallaxEffect();
+  
